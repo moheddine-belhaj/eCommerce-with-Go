a10282c72b2b88ec5a3451c8cd3192cfe72fb70d
moheddine-belhaj <moheddinebelhaj@gmail.com>
2024-11-28 19:59:46 +0100

    set controllers func

---
+++ controllers/address.go
@@ -0,0 +1,20 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func AddAdress() gin.HandlerFunc{
+	
+}
+
+func EditHomeAdress() gin.HandlerFunc{
+
+}
+
+func EditWorkAdress() gin.HandlerFunc {
+
+
+}
+
+func DeleteAdress() gin.HandlerFunc{
+	
+}
