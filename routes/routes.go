9a3b178568755505809006d301718c7eab70fedb
moheddine-belhaj <moheddinebelhaj@gmail.com>
2024-12-17 17:43:02 +0100

    add GetAll()

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/moheddine-belhaj/eCommerce-with-Go/controllers"
+
 )
 
 func UserRoutes(incomingRoutes *gin.Engine){
@@ -10,6 +11,7 @@ func UserRoutes(incomingRoutes *gin.Engine){
 	incomingRoutes.POST("/users/login",controllers.Login())
 	incomingRoutes.POST("/admin/addproduct",controllers.ProductViewAdmin())
 	incomingRoutes.GET("/users/productview",controllers.SearchProduct())
+	incomingRoutes.GET("/admin/all",controllers.GetAll())
 	incomingRoutes.GET("/users/search",controllers.SearchProductByQuerry())
 
 }
