4ffc5f24fa415fb65a258940d2baa3b1d05afa4c
moheddine-belhaj <moheddinebelhaj@gmail.com>
2024-12-17 20:14:25 +0100

    Update for AddProductsFromFile()

--- routes/routes.go
+++ routes/routes.go
@@ -6,12 +6,16 @@ import (
 
 )
 
+var filename = "./product.json"
+
+
 func UserRoutes(incomingRoutes *gin.Engine){
 	incomingRoutes.POST("/users/signup", controllers.SignUp())
 	incomingRoutes.POST("/users/login",controllers.Login())
 	incomingRoutes.POST("/admin/addproduct",controllers.ProductViewAdmin())
+	incomingRoutes.POST("/admin/AddProductsFromFile",controllers.AddProductsFromFile(filename))
 	incomingRoutes.GET("/users/productview",controllers.SearchProduct())
 	incomingRoutes.GET("/admin/all",controllers.GetAll())
 	incomingRoutes.GET("/users/search",controllers.SearchProductByQuerry())
 
-}
+}	
